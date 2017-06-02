b1dd902ec439d68b4c00276258b3d61f14273a40
Yishai8 <yishai_neeman@yahoo.com>
2017-06-02 19:10:44 +0300

    first

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Control.h"
+#include "Graphics.h"
+
+using namespace std;
+
+
+// Main function. Creates and sets controls, adds them to panel and runs event engine on panel.
+void main() {
+
+}
+
+
